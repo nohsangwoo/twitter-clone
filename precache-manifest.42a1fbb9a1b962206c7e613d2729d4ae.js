f7201d5ff21c7971a302616679de6cf40297d5e1
nohsangwoo <nsgr12@naver.com>
2020-09-13 03:00:34 +0900

    Updates

---
+++ precache-manifest.42a1fbb9a1b962206c7e613d2729d4ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86cf6b5e41bd15ad46fdac25b5b81fcb",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "9f9bf131a0e2cf75fc91",
+    "url": "/twitter-clone/static/css/main.9333dc4c.chunk.css"
+  },
+  {
+    "revision": "2337cf2b934a8dac0ada",
+    "url": "/twitter-clone/static/js/2.74572f31.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/twitter-clone/static/js/2.74572f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f9bf131a0e2cf75fc91",
+    "url": "/twitter-clone/static/js/main.0ee9956c.chunk.js"
+  },
+  {
+    "revision": "516be1713cb27948156e",
+    "url": "/twitter-clone/static/js/runtime-main.64705aa1.js"
+  }
+]);
